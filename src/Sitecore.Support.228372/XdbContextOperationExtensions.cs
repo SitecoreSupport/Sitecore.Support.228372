5c14bb6450cee82c5f75bfdcf3076903e7f46b22
AnnaAkimova <aak@sitecore.net>
2018-07-06 11:43:51 +0300

    Fix commit

--- src/Sitecore.Support.228372/XdbContextOperationExtensions.cs
+++ src/Sitecore.Support.228372/XdbContextOperationExtensions.cs
@@ -1,11 +1,15 @@
-﻿namespace Sitecore.XConnect
+﻿namespace Sitecore.Support.XConnect
 {
   using Sitecore.XConnect.Operations;
+  using Sitecore.XConnect;
   using System;
+  using Sitecore.XConnect.Collection.Model;
+  using System.Collections.Generic;
+  using Sitecore.XConnect.Client;
 
   public static class XdbContextOperationExtensions
   {
-    public static RightToBeForgottenOperation ExecuteRightToBeForgotten(this IXdbContext context, IEntityReference<Contact> contact)
+    public static RightToBeForgottenOperation ExecuteRightToBeForgotten_Fixed(this IXdbContext context, IEntityReference<Contact> contact)
     {
       if (context == null)
       {
@@ -17,8 +21,25 @@
       }
       RightToBeForgottenOperation operation = new RightToBeForgottenOperation(contact);
       context.RegisterOperation(operation);
+      #region Added code
+      // The fix: remove Subcriptions from ListSubscriptions facet of anonymized contact
+      RemoveContactListSubscriptions(context, contact);
+      #endregion
       return operation;
     }
+    #region Added code
+    private static void RemoveContactListSubscriptions(IXdbContext context, IEntityReference<Contact> contactReference)
+    {
+      Sitecore.XConnect.Contact contact = context.Get<Sitecore.XConnect.Contact>(contactReference, new ExpandOptions(ListSubscriptions.DefaultFacetKey));
+      var listSubscriptionsFacet = contact.GetFacet<ListSubscriptions>(ListSubscriptions.DefaultFacetKey);
+      if (listSubscriptionsFacet != null)
+      {
+        listSubscriptionsFacet.Subscriptions = new List<ContactListSubscription>();
+        context.SetFacet(contact, ListSubscriptions.DefaultFacetKey, listSubscriptionsFacet);
+        context.Submit();
+      }
+    }
+    #endregion 
 
   }
 }
