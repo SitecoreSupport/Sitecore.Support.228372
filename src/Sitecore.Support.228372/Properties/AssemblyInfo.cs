17d4359c748cda149d1c15179e63888a14e98153
AnnaAkimova <aak@sitecore.net>
2018-07-06 11:26:03 +0300

    Init commit

---
+++ src/Sitecore.Support.228372/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.228372")]
+[assembly: AssemblyProduct("Sitecore.Support.228372")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.228372/XdbContextOperationExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.XConnect
+{
+  using Sitecore.XConnect.Operations;
+  using System;
+
+  public static class XdbContextOperationExtensions
+  {
+    public static RightToBeForgottenOperation ExecuteRightToBeForgotten(this IXdbContext context, IEntityReference<Contact> contact)
+    {
+      if (context == null)
+      {
+        throw new ArgumentNullException("context");
+      }
+      if (contact == null)
+      {
+        throw new ArgumentNullException("contact");
+      }
+      RightToBeForgottenOperation operation = new RightToBeForgottenOperation(contact);
+      context.RegisterOperation(operation);
+      return operation;
+    }
+
+  }
+}
